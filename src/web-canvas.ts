cb6f3d94f21a95d30e25b15f13d00eb2a34cb982
justin willis <jgw9617@gmail.com>
2020-01-02 05:54:37 +0000

    feat(); first solid impl

--- src/web-canvas.ts
+++ src/web-canvas.ts
@@ -7,15 +7,20 @@ export class WebCanvas extends LitElement {
 
   @property() height: number;
   @property() width: number;
-  @property() color: string;
+  @property() color: string = 'red';
+  @property() mode: string = 'pen';
+
 
   @property() canvas: HTMLCanvasElement;
   @property() canvasContext: CanvasRenderingContext2D;
 
-  @property() drawing: boolean;
+  @property({type : Boolean}) drawing: boolean = true;
 
-  mousePos: { x: number, y: number };
-  lastPos: { x: number, y: number };
+  @property({type : Boolean}) showcontrols: boolean = false;
+
+  mousePos: any;
+  lastPos: any;
+  rect: DOMRect;
 
   firstUpdated() {
     this.setupCanvas();
@@ -26,6 +31,8 @@ export class WebCanvas extends LitElement {
 
     this.canvas.width = this.width;
     this.canvas.height = this.height;
+
+    this.rect = this.canvas.getBoundingClientRect();
 
     this.canvasContext = (this.canvas.getContext('2d', {
       desynchronized: true
@@ -48,6 +55,8 @@ export class WebCanvas extends LitElement {
     }
 
     this.setupDrawingEvents();
+
+    this.renderCanvas();
   }
 
   setupDrawingEvents() {
@@ -56,11 +65,116 @@ export class WebCanvas extends LitElement {
     this.mousePos = { x: 0, y: 0 };
 
     this.canvas.addEventListener("pointerdown", (e) => {
-      console.log(e);
-    })
+      if (e.button !== 2) {
+        this.lastPos = this.getMousePos(e);
+
+        if (e.pointerType !== 'touch') {
+          this.drawing = true;
+        }
+      }
+    });
+
+    this.canvas.addEventListener("pointermove", (e: PointerEvent) => {
+      this.mousePos = this.getMousePos(e);
+
+      if (e.pointerType === "touch") {
+        this.drawing = true;
+      }
+    });
+
+    this.canvas.addEventListener("pointerup", (e: PointerEvent) => {
+      this.drawing = false;
+      this.lastPos = null;
+    });
+  }
+
+  getMousePos(event: PointerEvent) {
+    return {
+      x: event.clientX - this.rect.left,
+      y: event.clientY - this.rect.top,
+      width: event.width,
+      type: event.pointerType,
+      ctrlKey: event.ctrlKey,
+      pressure: event.pressure,
+      button: event.button,
+      buttons: event.buttons
+    };
+  }
+
+  renderCanvas() {
+    if (this.drawing !== false && this.mode === 'pen') {
+
+      if (this.lastPos) {
+        this.canvasContext.globalCompositeOperation = 'source-over';
+        this.canvasContext.beginPath();
+        this.canvasContext.moveTo(this.lastPos.x, this.lastPos.y);
+        this.canvasContext.lineTo(this.mousePos.x, this.mousePos.y);
+
+        if (this.mousePos.type === 'pen') {
+          let tweakedPressure = this.mousePos.pressure * 6;
+          this.canvasContext.lineWidth = this.mousePos.width + tweakedPressure;
+
+          if (this.mousePos.buttons === 32 && this.mousePos.button === -1) {
+            // eraser
+
+            this.canvasContext.globalCompositeOperation = 'destination-out';
+            this.canvasContext.beginPath();
+            this.canvasContext.moveTo(this.lastPos.x, this.lastPos.y);
+            this.canvasContext.lineTo(this.mousePos.x, this.mousePos.y);
+
+            this.canvasContext.lineWidth = 60;
+
+            this.canvasContext.stroke();
+            this.canvasContext.closePath();
+
+            this.lastPos = this.mousePos;
+          }
+        }
+        else if (this.mousePos.type !== 'mouse' && this.mousePos.type !== 'pen') {
+          this.canvasContext.lineWidth = this.mousePos.width - 20;
+        }
+        else if (this.mousePos.type !== 'touch' && this.mousePos.type !== 'pen') {
+          this.canvasContext.lineWidth = 10;
+        }
+
+        this.canvasContext.stroke();
+        this.canvasContext.closePath();
+      }
+
+      this.lastPos = this.mousePos;
+
+    }
+    else if (this.drawing !== false && this.mode === 'erase') {
+      this.canvasContext.globalCompositeOperation = 'destination-out';
+      this.canvasContext.beginPath();
+      this.canvasContext.moveTo(this.lastPos.x, this.lastPos.y);
+      this.canvasContext.lineTo(this.mousePos.x, this.mousePos.y);
+
+      if (this.mousePos.type === 'mouse') {
+        this.canvasContext.lineWidth = 30;
+      }
+
+      this.canvasContext.stroke();
+      this.canvasContext.closePath();
+
+      this.lastPos = this.mousePos;
+    }
+
+    requestAnimationFrame(() => this.renderCanvas());
   }
 
   render() {
-    return html`<canvas></canvas>`;
+    return html`
+      <div>
+        <canvas></canvas>
+
+        ${
+          this.showcontrols ? html `
+          <div id="controls">
+            <button>erase</button>
+          </div>` : null
+        }
+      </div>
+    `;
   }
 }
